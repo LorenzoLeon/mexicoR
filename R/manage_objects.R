b5e6d83f845462a8b4318ae1aaf27fe341374b83
Javier Martinez Rodriguez <javierwolfsangel@gmail.com>
2021-08-12 09:47:09 -0500

    .

---
+++ R/manage_objects.R
@@ -0,0 +1,37 @@
+u <- Sys.setlocale("LC_ALL", "es_ES.UTF-8")
+
+#' Desagrupar por fila en caso de haber usado rowwise
+#'
+#' @param data dataframe agrupado por fila
+#'
+#' @return dataframe sin agrupación
+#' @export
+unrowwise_df <- function(data) {
+  
+  class(data) <- c("tbl_df", "data.frame")
+  
+  return(data)
+  
+}
+
+#' Guardar base de datos en nuevo objeto
+#'
+#' @param data cualquier objeto
+#' @param nombre string que se le asigna como nombre al objeto. Si no se 
+#' asigna ninguno, se guardará como obj_guardado .
+#'
+#' @return el mismo objeto y guarda un nuevo objeto con el nombre de bd_guardada
+#' @export
+#'
+#' @examples
+guardar <- function(data, nombre = "obj_guardado") {
+  
+  assign(nombre, 
+         data,
+         envir = .GlobalEnv)
+
+  
+  return(data)
+  
+}
+  
